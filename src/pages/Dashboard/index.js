2742727ea26c4c0005ca8215533c1eb0499ec40b
Erik Garces Matos <p22.matos@webmotors.com.br>
2020-09-01 00:07:04 -0300

    feat(master): Altera texto

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,7 +5,7 @@ import { Container } from './styles';
 export default function Dashboard() {
   return (
     <Container>
-      <h1>bem vindo ao template de app ReactJS.By @Erik Matos</h1>
+      <h1>Bem vindo ao template de app ReactJS.By @Erik Matos</h1>
     </Container>
   );
 }
